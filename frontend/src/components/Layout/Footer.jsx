eedd50219f2cb573386a36ba50ba9b65b4dc6846
blhere <stuffsams@gmail.com>
2025-12-17 14:47:42 +0530

    Create Job portal

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import {Context} from "../../main"
+import {Link} from "react-router-dom"
+import { FaGithub , FaLinkedin} from "react-icons/fa"
+import { SiLeetcode } from "react-icons/si";
+import { RiInstagramFill} from "react-icons/ri"
+function Footer() {
+  const {isAuthorized}  = useContext(Context)
+  return (
+    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
+<div>&copy; All Rights Reserved.</div>
+    </footer>
+  )
+}
+
+export default Footer
